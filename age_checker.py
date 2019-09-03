b398196e70bd1a6526b73059516d50caa4547b3d
Adil Shaik <shaikadil95@gmail.com>
2019-09-03 13:19:09 +0200

    Update age_checker.py

--- age_checker.py
+++ age_checker.py
@@ -1,11 +1,4 @@
 age = int(raw_input("Please Enter your Age:"))
-if age >= 18:
-    print("You can visit the site!")
-else:
-    print("You are kiddo!")
-	
-
-age = int(raw_input("Please Enter your Age:"))
 if age >= 18 and age <=65:
     print("You can visit the site!")
 elif age >=65:
